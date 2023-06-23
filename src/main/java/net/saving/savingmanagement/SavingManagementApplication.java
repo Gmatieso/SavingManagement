54c25c034a770d462d709cdd1fd9e8984d34aee4
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-23 17:20:30 +0300

    Added new files

---
+++ src/main/java/net/saving/savingmanagement/SavingManagementApplication.java
@@ -0,0 +1,13 @@
+package net.saving.savingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/saving/savingmanagement/SavingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package net.saving.savingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
