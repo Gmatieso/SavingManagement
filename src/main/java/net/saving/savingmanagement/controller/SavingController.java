fa8a03b3b6e6160ec5dde08b0f8d421f0c3120b1
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 13:03:49 +0300

    GET a list of all savings Done

--- src/main/java/net/saving/savingmanagement/controller/SavingController.java
+++ src/main/java/net/saving/savingmanagement/controller/SavingController.java
@@ -1,2 +1,29 @@
-package net.saving.savingmanagement.controller;public class SavingController {
+package net.saving.savingmanagement.controller;
+
+
+import net.saving.savingmanagement.model.Saving;
+import net.saving.savingmanagement.repository.CustomerRepository;
+import net.saving.savingmanagement.repository.SavingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+//defining a base url of all the api
+@RequestMapping("/api/v1/saving")
+public class SavingController {
+
+    @Autowired
+    private SavingRepository savingRepository;
+
+    //GET a list of all savings
+    @GetMapping
+    public List<Saving> getAllSaving() {
+        return  savingRepository.findAll();
+    }
 }
