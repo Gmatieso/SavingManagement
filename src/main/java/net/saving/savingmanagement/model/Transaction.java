13ee8a7a988338a1a2d569206803388ccafae103
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 05:53:20 +0300

    Transaction model done

--- src/main/java/net/saving/savingmanagement/model/Transaction.java
+++ src/main/java/net/saving/savingmanagement/model/Transaction.java
@@ -1,2 +1,31 @@
-package net.saving.savingmanagement.model;public class Transaction {
+package net.saving.savingmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="transactions")
+
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name ="transaction_date")
+    private String transactionDate;
+
+    @Column(name ="payment_method")
+    private String paymentMethod;
+
+
+    @Column(name="amount")
+    private Integer amount;
 }
