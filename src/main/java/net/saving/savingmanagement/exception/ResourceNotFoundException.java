066339dc784bdf480e2c94ba8b48145ef35f5a67
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 07:09:03 +0300

    ResourceNotFoundException Done

--- src/main/java/net/saving/savingmanagement/exception/ResourceNotFoundException.java
+++ src/main/java/net/saving/savingmanagement/exception/ResourceNotFoundException.java
@@ -1,2 +1,11 @@
-package net.saving.savingmanagement.exception;public class ResourceNotFoundException {
+package net.saving.savingmanagement.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
