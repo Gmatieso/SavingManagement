d869f2d9f6f99c1fd2e17265e281fdb0d531a371
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 06:28:27 +0300

    Saving model done

--- src/main/java/net/saving/savingmanagement/model/Saving.java
+++ src/main/java/net/saving/savingmanagement/model/Saving.java
@@ -1,2 +1,25 @@
-package net.saving.savingmanagement.model;public class saving {
+package net.saving.savingmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="savings")
+public class Saving {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name ="product_name")
+    private String productName;
+
+    @Column(name="amount")
+    private Integer amount;
 }
