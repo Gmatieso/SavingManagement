2f1eeaca705afbb065a529befbdda9ecc024e69c
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 15:21:33 +0300

    build update personalSaving REST API Done

--- src/main/java/net/saving/savingmanagement/controller/PersonalSavingController.java
+++ src/main/java/net/saving/savingmanagement/controller/PersonalSavingController.java
@@ -40,5 +40,21 @@ public class PersonalSavingController {
         return  ResponseEntity.ok(personalSaving);
     }
 
+    //build update personalSaving REST API
+    @PutMapping("{id}")
+    public  ResponseEntity<PersonalSaving> updatePersonalSaving(@PathVariable long id, @RequestBody PersonalSaving personalSavingDetails) {
+        PersonalSaving updatePersonalSaving = personalSavingRepository.findById(id)
+                //throw and exception if id not found
+                .orElseThrow(()-> new ResourceNotFoundException("Personal Saving does not exit with id:" + id));
+        //else update customer if id is found
+        updatePersonalSaving.setName(personalSavingDetails.getName());
+        updatePersonalSaving.setTotalSavings(personalSavingDetails.getTotalSavings());
+
+        //save this object to the database
+        personalSavingRepository.save(updatePersonalSaving);
+        //return updated info to the client
+        return ResponseEntity.ok(updatePersonalSaving);
+    }
+
 
 }
