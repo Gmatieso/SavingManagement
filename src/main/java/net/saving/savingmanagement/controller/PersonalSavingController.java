3ba4dc108bcbd75a6e106426e2cd122b2115c277
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 14:35:32 +0300

    Wiring our repository with our controller done

--- src/main/java/net/saving/savingmanagement/controller/PersonalSavingController.java
+++ src/main/java/net/saving/savingmanagement/controller/PersonalSavingController.java
@@ -1,5 +1,7 @@
 package net.saving.savingmanagement.controller;
 
+import net.saving.savingmanagement.repository.PersonalSavingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 //defining a base url of all the api
 @RequestMapping("/api/v1/personalsaving")
 public class PersonalSavingController {
-
+    //wiring our repository with our controller
+    @Autowired
+    private PersonalSavingRepository personalSavingRepository;
 
 }
