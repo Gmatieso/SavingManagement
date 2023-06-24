bba34bcf5b67eec0c3847ead9d8aeace440f3e73
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 08:47:53 +0300

    build create customer REST API done

--- src/main/java/net/saving/savingmanagement/controller/CustomerController.java
+++ src/main/java/net/saving/savingmanagement/controller/CustomerController.java
@@ -3,13 +3,12 @@ package net.saving.savingmanagement.controller;
 import net.saving.savingmanagement.model.Customer;
 import net.saving.savingmanagement.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
+@CrossOrigin("*")
 @RestController
 //defining a base url of all the api
 @RequestMapping("/api/v1/customer")
@@ -22,4 +21,13 @@ public class CustomerController {
     public List<Customer> getAllCustomer(){
         return customerRepository.findAll();
     }
+
+    //build create customer REST API
+
+    @PostMapping
+    public Customer createEmployee(@RequestBody Customer customer){
+        //returns saved customer object
+        return customerRepository.save(customer);
+    }
+
 }
