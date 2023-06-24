bab3a27b26f9893cd5c6fc21aeb219c642d94421
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 09:16:41 +0300

    build update customer REST API Done

--- src/main/java/net/saving/savingmanagement/controller/CustomerController.java
+++ src/main/java/net/saving/savingmanagement/controller/CustomerController.java
@@ -20,13 +20,13 @@ public class CustomerController {
 
     //GET a list of all customers
     @GetMapping
-    public List<Customer> getAllCustomer(){
+    public List<Customer> getAllCustomer() {
         return customerRepository.findAll();
     }
 
     //build create customer REST API
     @PostMapping
-    public Customer createEmployee(@RequestBody Customer customer){
+    public Customer createEmployee(@RequestBody Customer customer) {
         //returns saved customer object
         return customerRepository.save(customer);
     }
@@ -35,8 +35,23 @@ public class CustomerController {
     @GetMapping("{id}")
     public ResponseEntity<Customer> getCustomerById(@PathVariable long id) {
         Customer customer = customerRepository.findById(id)
-                .orElseThrow(()-> new ResourceNotFoundException("Customer does not exist with id:"+ id));
+                .orElseThrow(() -> new ResourceNotFoundException("Customer does not exist with id:" + id));
         return ResponseEntity.ok(customer);
     }
 
+    //build update customer REST API
+    @PutMapping("{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable long id, @RequestBody Customer customerDetails) {
+        Customer updateCustomer = customerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer does not exist with id:" + id));
+        updateCustomer.setName(customerDetails.getName());
+        updateCustomer.setPhoneNumber(customerDetails.getPhoneNumber());
+        updateCustomer.setEmailId(customerDetails.getEmailId());
+        updateCustomer.setMemberNumber(customerDetails.getMemberNumber());
+
+        //save this object to the database
+        customerRepository.save(updateCustomer);
+        //return updated info to the client
+        return ResponseEntity.ok(updateCustomer);
+    }
 }
