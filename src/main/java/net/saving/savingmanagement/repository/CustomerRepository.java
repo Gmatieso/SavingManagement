aa0aab89e518ce3980d91920dac4871c46eae605
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 07:39:45 +0300

    CustomerRepository interface Done

--- src/main/java/net/saving/savingmanagement/repository/CustomerRepository.java
+++ src/main/java/net/saving/savingmanagement/repository/CustomerRepository.java
@@ -1,2 +1,8 @@
-package net.saving.savingmanagement.repository;public class CustomerRepository {
+package net.saving.savingmanagement.repository;
+
+import net.saving.savingmanagement.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    //Accepts all crud database methods
 }
