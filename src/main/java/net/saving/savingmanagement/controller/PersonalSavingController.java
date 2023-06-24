2f8377a8d9b52639dfc0eebeb389c639ea57eb91
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 14:33:41 +0300

    Defining a base url of the personalsaving api done

--- src/main/java/net/saving/savingmanagement/controller/PersonalSavingController.java
+++ src/main/java/net/saving/savingmanagement/controller/PersonalSavingController.java
@@ -1,2 +1,14 @@
-package net.saving.savingmanagement.controller;public class PersonalSavingController {
+package net.saving.savingmanagement.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+//defining a base url of all the api
+@RequestMapping("/api/v1/personalsaving")
+public class PersonalSavingController {
+
+
 }
