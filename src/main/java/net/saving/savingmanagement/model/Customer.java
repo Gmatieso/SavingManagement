aec9717adeb36462c225a32fcecc53e10657048e
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 05:43:06 +0300

    Customer Model Done

--- src/main/java/net/saving/savingmanagement/model/Customer.java
+++ src/main/java/net/saving/savingmanagement/model/Customer.java
@@ -1,2 +1,32 @@
-package net.saving.savingmanagement.model;public class customer {
+package net.saving.savingmanagement.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name ="name")
+    private String Name;
+
+    @Column(name ="phone_number")
+    private Integer phoneNumber;
+
+    @Column(name="email_id")
+    private String emailId;
+
+    @Column(name="member_number")
+    private Integer memberNumber;
 }
