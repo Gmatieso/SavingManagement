c38dbff3753d7680b780b3d954c6641e4962e95b
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 14:55:14 +0300

    annoted method getAllPersonalSaving with @GetMapping

--- src/main/java/net/saving/savingmanagement/controller/PersonalSavingController.java
+++ src/main/java/net/saving/savingmanagement/controller/PersonalSavingController.java
@@ -4,6 +4,7 @@ import net.saving.savingmanagement.model.PersonalSaving;
 import net.saving.savingmanagement.repository.PersonalSavingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class PersonalSavingController {
     private PersonalSavingRepository personalSavingRepository;
 
     //GET a list of all personalsaving
+    @GetMapping
     public List<PersonalSaving> getAllPersonalSaving() {
         return personalSavingRepository.findAll();
     }
