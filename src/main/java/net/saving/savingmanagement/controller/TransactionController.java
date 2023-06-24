a059ee3433fc2d49eca85c3b1f4c0290de3d30c8
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 17:07:43 +0300

    Wiring our repository with our controller done

--- src/main/java/net/saving/savingmanagement/controller/TransactionController.java
+++ src/main/java/net/saving/savingmanagement/controller/TransactionController.java
@@ -1,5 +1,7 @@
 package net.saving.savingmanagement.controller;
 
+import net.saving.savingmanagement.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 //defining a base url of all the api
 @RequestMapping("/api/v1/transaction")
 public class TransactionController {
+
+    //wiring our repository with our controller
+    @Autowired
+    private TransactionRepository transactionRepository;
 }
