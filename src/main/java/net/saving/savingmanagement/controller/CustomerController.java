4d7a39748d182d90b26008a76f929f3f88c099bb
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 09:03:55 +0300

    get customer by id REST API done

--- src/main/java/net/saving/savingmanagement/controller/CustomerController.java
+++ src/main/java/net/saving/savingmanagement/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package net.saving.savingmanagement.controller;
 
+import net.saving.savingmanagement.exception.ResourceNotFoundException;
 import net.saving.savingmanagement.model.Customer;
 import net.saving.savingmanagement.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,11 +25,18 @@ public class CustomerController {
     }
 
     //build create customer REST API
-
     @PostMapping
     public Customer createEmployee(@RequestBody Customer customer){
         //returns saved customer object
         return customerRepository.save(customer);
+    }
+
+    //build get customer by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Customer does not exist with id:"+ id));
+        return ResponseEntity.ok(customer);
     }
 
 }
