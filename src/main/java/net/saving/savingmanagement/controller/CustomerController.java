dd7758f69171af751cc0e9ed4d690e6a1ee4d483
Gmatieso <matiesogeoffrey@gmail.com>
2023-06-24 07:54:43 +0300

    GET a list of all customers  Done

--- src/main/java/net/saving/savingmanagement/controller/CustomerController.java
+++ src/main/java/net/saving/savingmanagement/controller/CustomerController.java
@@ -1,2 +1,25 @@
-package net.saving.savingmanagement.controller;public class CustomerController {
+package net.saving.savingmanagement.controller;
+
+import net.saving.savingmanagement.model.Customer;
+import net.saving.savingmanagement.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+//defining a base url of all the api
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    //GET a list of all customers
+    @GetMapping
+    public List<Customer> getAllCustomer(){
+        return customerRepository.findAll();
+    }
 }
